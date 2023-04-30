8b71b694c07e11ea4d73c6639799bf9876318038
huxuanping <61032342+huxuanping@users.noreply.github.com>
2023-04-30 15:14:35 +0800

    Update 乐猪TV.js

--- dr2/乐猪TV.js
+++ dr2/乐猪TV.js
@@ -6,8 +6,8 @@ var rule = {
     filterable:1,//是否启用分类筛选,
     filter_url:'_fypage_desc_{{fl.by}}_0_0___',
     filter: {"1":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"2":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"4":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"3":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"14":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}],"15":[{"key":"by","name":"排序","value":[{"n":"全部","v":"time"},{"n":"时间","v":"time"},{"n":"人气","v":"hits"},{"n":"评分","v":"score"}]}]},
-    // searchUrl:'/search-pg-fypage-wd-**.html',
-    searchUrl:'/index.php?m=vod-search#wd=**&search=;post',
+    searchUrl:'/search-pg-1-wd-**.html',
+    //searchUrl:'/index.php?m=vod-search#wd=**&search=;post',
     searchable:2,
     quickSearch:0,
     headers:{
